1e9cb19a3cb4c4efdf497ae7d465fda6e80c82f1
Truc Nguyen <vnussolution@gmail.com>
2018-06-06 11:16:57 -0700

    baseline project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>hello </h2>
+
+        <button> toggle </button>
+
+        <input type="text" />
+        <p> the length is</p>
+      </div>
+    );
+  }
+}
+export default App;
