8d49a11977967b657525191016cee2d10d5846e2
VNUS Solution <vnussolution@gmail.com>
2018-06-18 09:50:55 -0700

    use withRouter

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,17 @@
 import React from "react";
-
+import { withRouter } from "react-router-dom";
 import classes from "./Post.css";
 
-const post = props => (
-  <article className={classes.Post} onClick={props.clicked}>
-    <h1>{props.title}</h1>
-    <div className={classes.Info}>
-      <div className={classes.Author}>{props.author}</div>
-    </div>
-  </article>
-);
+const post = props => {
+  console.log(props);
 
-export default post;
+  return (
+    <article className={classes.Post} onClick={props.clicked}>
+      <h1>{props.title}</h1>
+      <div className={classes.Info}>
+        <div className={classes.Author}>{props.author}</div>
+      </div>
+    </article>
+  );
+};
+export default withRouter(post);
