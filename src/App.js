e46fe89dbfab8d129aae3b16d5df9368fe5dd7da
VNUS Solution <vnussolution@gmail.com>
2018-06-30 08:05:02 -0700

    learning redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
+
+import Counter from "./containers/Counter/Counter";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>hello </h2>
-
-        <button> toggle </button>
-
-        <input type="text" />
-        <p> the length is</p>
+        <Counter />
       </div>
     );
   }
 }
+
 export default App;
