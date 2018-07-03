db606e8bece86a026dd61ae879544b7c9333dd5c
Truc Nguyen <vnussolution@gmail.com>
2018-07-03 13:44:17 -0700

    implement basic redux to burger

---
+++ src/store/reducer.js
@@ -0,0 +1,18 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+  switch ("key") {
+    case "value":
+      break;
+
+    default:
+      break;
+  }
+};
+
+export default reducer;
